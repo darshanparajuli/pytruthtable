f2f6aac7a34ab981e5f40f07c40ce30fe19c7a39
Darshan Parajuli <parajulidarshan@gmail.com>
2015-10-07 21:39:06 -0700

    update

--- truthtable.py
+++ truthtable.py
@@ -1,5 +1,10 @@
 # Written by Darshan Parajuli, 12/02/2014
 
+import itertools
+
+# valid operators
+operators = ['<>', '>', '|', '&', '!']
+
 # Binary Tree node
 class Node:
     def __init__(self, value):
@@ -7,7 +12,7 @@ class Node:
         self.right = None
         self.value = value
 
-# Evaluate a given proposition in tree form 
+# Evaluate a given proposition in tree form
 class TreeEvaluator:
     def __init__(self):
         self.valueTable = None
@@ -37,16 +42,14 @@ class TreeEvaluator:
             return a == b
         else:
             return None
-        
+
     # evaluate the tree for each given list of truth values
     def evaluate(self, tree):
         result = []
         for v in self.valueTable:
             value = dict()
-            i = 0
-            while i < len(self.operands):
+            for i in range(0, len(self.operands)):
                 value[self.operands[i]] = v[i]
-                i += 1
 
             a = self.evaluateHelper(tree, value)
             result.append(a)
@@ -57,18 +60,12 @@ class TreeEvaluator:
     def evaluateHelper(self, tree, valuepair):
         if tree.value in self.operands:
             return valuepair[tree.value]
-
         a = None
         if tree.left != None:
             a = self.evaluateHelper(tree.left, valuepair)
-            
         b = self.evaluateHelper(tree.right, valuepair)
-            
         op = tree.value
-        
         return self.evaluatePreposition(op, a, b)
-
-operators = ['<>', '>', '|', '&', '!']
 
 # check if user input has errors
 def hasErrors(expression):
@@ -90,20 +87,20 @@ def hasErrors(expression):
         if currOp in operators:
             if nextOp in operators:
                 return "consecutive operators"
-                    
+
         if currOp not in operators and currOp != '(' and currOp != ')':
             if nextOp not in operators and nextOp != '(' and nextOp != ')':
                 return "consecutive operands"
 
     import collections
     counter = collections.Counter(expression)
-    
+
     if '(' in expression or ')' in expression:
         if counter['('] > counter[')']:
             return "closing parenthesis missing"
         elif counter['('] < counter[')']:
             return "opening parenthesis missing"
-            
+
     return False
 
 # converts the given infix expression to postfix expression, i.e. 3 + 4 -> 3 4 +
@@ -112,7 +109,7 @@ def convertToPostFix(expression):
     stack = []
 
     order = ['(', '<>', '>', '|', '&', '!']
-    
+
     for i in expression:
         if i not in operators and i != '(' and i != ')':
             postFix.append(i)
@@ -130,14 +127,14 @@ def convertToPostFix(expression):
 
     while len(stack) > 0:
         postFix.append(stack.pop())
-        
+
     return postFix
 
 # build a tree for each column of the truth table with the given postfix expression
 def convertPostFixToTree(postFix):
     stack = []
     trees = []
-    
+
     for i in postFix:
         if i not in operators:
             stack.append(Node(i))
@@ -150,10 +147,10 @@ def convertPostFixToTree(postFix):
             node.right = a
             if b is not None:
                 node.left = b
-            
+
             stack.append(node)
             trees.append(node)
-            
+
     return trees
 
 # traverse the tree in-order so it displays the postfix data in infix i.e. 3 4 + -> 3 + 4
@@ -173,31 +170,71 @@ def displayResult(operands, table, exp, result):
     lengths = []
     for i in exp:
         l = len(i)
-        if l < 5:
-            l = 5;
-        lengths.append(l + 5)
-            
+        if l < 2:
+            l = 2;
+        lengths.append(l + 2)
+
     output = []
-    
+
     temp = ""
     for i in range(0, len(exp)):
         temp += exp[i].ljust(lengths[i])
     output.append(temp)
-    
+
     for i in table:
         temp = ""
         for j in range(0, len(i)):
             temp += str(i[j]).ljust(lengths[j])
         output.append(temp)
 
-        
+
     templen = len(table[0])
     for i in range(0, len(result)):
         for j in range(0, len(result[i])):
-            output[j + 1] += str(result[i][j]).ljust(lengths[i + templen])
-                
+            output[j + 1] += str(int(result[i][j])).ljust(lengths[i + templen])
+
     for i in output:
         print(i)
+
+
+def run(userInput, evaluator):
+    expression = list(userInput.split())
+    error = hasErrors(expression)
+
+    if error != False:
+        print("Error: " + error)
+    else:
+        postFix = convertToPostFix(expression)
+        # print('converted to postfix')
+
+        # print(str(postFix))
+
+        trees = convertPostFixToTree(postFix)
+        # print('converted to postfix tree')
+
+        operands = [i for i in postFix if i not in operators]
+        operands = list(set(operands))
+        operands.sort()
+
+        table = list(itertools.product([1, 0], repeat = len(operands)))
+        evaluator.setInitialValues(operands, table)
+        # print('product complete')
+
+        exp = []
+        result = []
+        for i in trees:
+            a = []
+            getExpression(i, a)
+            # print('expression: {}'.format(a))
+            b = evaluator.evaluate(i)
+            # print('evaluation complete')
+            exp.append(' '.join(a))
+            result.append(b)
+
+        # print('done')
+        # print("displaying results")
+        displayResult(operands, table, exp, result)
+
 
 # main function, program starts here
 def main():
@@ -213,44 +250,19 @@ def main():
     print("*Proper input example: ( p & q ) | ( ! r )                                                *")
     print("*******************************************************************************************\n")
 
-    inputPrompt = "Enter a propositional sentence (type 'quit' to quit): "
-    
+    inputPrompt = "Enter a propositional sentence (enter 'exit' to quit): "
+
     userInput = input(inputPrompt)
 
     evaluator = TreeEvaluator()
-    
-    while userInput != "quit":
-        expression = list(userInput.split())
-        error = hasErrors(expression)
-        
-        if error != False:
-            print("Error: " + error)
-        else:
-            postFix = convertToPostFix(expression)
-            trees = convertPostFixToTree(postFix)
-        
-            operands = [i for i in postFix if i not in operators]
-            operands = list(set(operands))
-            operands.sort()
 
-            import itertools
-            table = list(itertools.product([True, False], repeat = len(operands)))
-            evaluator.setInitialValues(operands, table)
-            
-            exp = []
-            result = []
-            for i in trees:
-                a = []
-                getExpression(i, a)
-                b = evaluator.evaluate(i)
-                exp.append(' '.join(a))
-                result.append(b)
-
-            displayResult(operands, table, exp, result)
-
+    while userInput != "exit":
+        run(userInput, evaluator)
         userInput = input(inputPrompt)
 
     print("Bye!")
 
 if __name__ == "__main__":
+    # test_input = "a | b | c | d | e | f | g | h"
+    # run(test_input, TreeEvaluator())
     main()
